5735d4088ad63a2bcb2c167fc3e1abaee1052570
Minh Tri <minhtrivo2005gg@gmail.com>
2025-10-04 17:14:59 +0700

    done UI shopping/cart page

--- app/shopping/cart/page.tsx
+++ app/shopping/cart/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 const Page = () =>{
     return (
         <div className="rounded-3xl bg-white flex gap-2 ">
             {/* Table */}
-            <div className="w-[70%] p-[20px] ">
-                <h1 className="text-3xl text-center">Giỏ hàng của Bạn [5]</h1>
+            <div className="w-[70%] px-[20px] py-[50px]">
+                <h1 className="text-3xl text-center mb-[30px] ">Giỏ hàng của Bạn [5]</h1>
                 <table className="table-auto border-collapse border border-gray-400 w-full text-sm ">
-                    <thead className="bg-green-600 text-white">
+                    <thead className="bg-[#00c28dfc] text-white">
                         <tr>
                         <th className="border border-gray-400 px-4 py-2">Product</th>
                         <th className="border border-gray-400 px-4 py-2">Quantity</th>
@@ -52,7 +54,15 @@ const Page = () =>{
 
 
             {/* Thanh toan */}
-            <div className="w-[30%]"></div>
+            <div className="w-[30%] px-[50px]   py-[100px]  ">
+                <div className="flex justify-between mb-[20px] text-2xl">
+                    <div>Tổng giá trị</div>
+                    <div>50000 vnd</div>
+                </div>
+                <Link className=" text-black bg-green-800" href={"/shopping/checkout"}>
+                    <div className="bg-[#00c28dfc] text-black rounded-sm py-[10px] text-center font-medium">THANH TOÁN</div>
+                </Link>
+            </div>
             
 
         </div>
